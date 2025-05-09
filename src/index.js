3050f36b898d9ce679eb31f5d899b9489edc5818
Rajiv Mandal <rajivmandal341@gmail.com>
2025-05-10 00:01:17 +0530

    commitAt12_01

--- src/index.js
+++ src/index.js
@@ -2,28 +2,13 @@
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import connectToDatabase from "./db/icabDb.js";
-
+import express from "express";
 dotenv.config({ path:'./env' });
-
-connectToDatabase();
-/*import express from 'express';
 const app = express();
-(async ()=>{
-    try{
-   await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`){
-    app.on('error', (error) => {
-        console.error('MongoDB connection error:', error);
-        throw error;
-    })
-    
-    app.listen(process.env.PORT,() =>{
-        console.log(`Server is running on port ${process.env.PORT}`);
-        {process.env.PORT}
-    })
-   }
-    }catch(error){
-        console.log(error.message);
-        throw error;
+connectToDatabase().then(() =>{
+    app.listen(process.env.PORT || 5000),() => {
+        console.log(`Server is running ar port ${process.env.PORT || 5000}`);
     }
-})()
-    */
+}).catch((err) => {
+    console.log("Error in connecting to database",err.message)
+})
